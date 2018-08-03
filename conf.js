0a62c025b10f318ee560cbc1df33876c7a18b9c3
pgwozdecki <36311038+pgwozdecki@users.noreply.github.com>
2018-08-03 19:01:48 +0200

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,28 +2,22 @@ var helpers = require('./features/support/helpers');
 
 exports.config = {
 
-    // setting to launch tests directly without selenium server
     directConnect : true,
-    // address of running selenium server
+
     seleniumAddress : 'http://localhost:4444/wd/hub',
 
-    // setting time outs
     getPageTimeOut : 200000,
     allScriptsTimeout : 500000,
 
-    // setting framework
     framework : 'custom',
     frameworkPath : require.resolve('protractor-cucumber-framework'),
-
-    // setting protractor to ignore uncaught exceptions to take care by protractor-cucumber-framework
+    
     ignoreUncaughtExceptions : true,
 
-    // configuration parameters
     params: {
         testEnv: 'test'
     },
 
-    // browser to launch tests
     capabilities : {
         browserName : 'chrome',
         chromeOptions : {
@@ -31,23 +25,17 @@ exports.config = {
         }
     },
 
-    // Specify Test Files
-    //
-    // Define which tests should execute
     specs : [
         'features/*.feature'
     ],
 
-    //Define which tests should be excluded from execution.
     exclude : [
-        // 'features/home.feature'
+        // if needed
     ],
 
-    // Set log level and enables colors for log output
     logLevel : 'verbose',
     coloredLogs : true,
 
-    // arguments to cucumber.js
     cucumberOpts : {
         require : [
             'features/support/env.js',
